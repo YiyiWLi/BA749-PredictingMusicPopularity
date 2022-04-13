10f900cab8cbbb1b8f475bf7bd575f1af5f08e2d
YiyiWLi <cyiyiwang@gmail.com>
2022-04-13 12:56:39 -0700

    Apr13_Yi

---
+++ Music.R
@@ -0,0 +1,5 @@
+#music_data = read.csv("data/music_genre.csv", na.strings=c("?"))
+#music_data = na.omit(music_data)
+#music_select = music_data[,c(4:9,11:15,17:18)]
+#music_select = music_select[-which(music_select[,4]==-1),]
+#write.csv(music_select,"data/music_selection.csv", row.names = FALSE)
